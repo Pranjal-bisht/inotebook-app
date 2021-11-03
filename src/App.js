ebc71639c8afd38a9ff8a42c46fb4cd3660420d1
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-11-03 14:21:24 +0530

    Created Navbar and routes using react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1> This is iNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
