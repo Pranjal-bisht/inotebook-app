5bd4d10db9c7f5fe8ec093816bb2744bac4cc98b
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-11-06 11:59:00 +0530

    Added Update Notes on edit in the UI

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -28,9 +28,13 @@ const NoteState = (props) => {
         'Content-Type': 'application/json',
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
       },
-      body: JSON.stringify({title, description, tag})
+      body: JSON.stringify({ title, description, tag })
     });
-     
+
+    const json = await response.json();
+    console.log(json)
+
+
     console.log("Adding a new note")
     const note = {
       "_id": "61322f119553781a8ca8d0e08",
@@ -43,7 +47,6 @@ const NoteState = (props) => {
     };
     setNotes(notes.concat(note))
   }
-
   // Delete a Note
   const deleteNote = async (id) => {
     // API Call
@@ -56,44 +59,38 @@ const NoteState = (props) => {
     });
     const json = response.json();
     console.log(json)
-
     console.log("Deleting the note with id" + id);
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
   }
-
   // Edit a Note
   const editNote = async (id, title, description, tag) => {
     // API Call 
-
-    
-          
-            
-    
-
-          
-    
-    
-  
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
       },
-      body: JSON.stringify({title, description, tag})
+      body: JSON.stringify({ title, description, tag })
     });
-    const json = response.json();
+    const json = await response.json();
+    console.log(json)
+
+    let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    setNotes(newNotes);
   }
+
   return (
     <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
       {props.children}
@@ -101,3 +98,5 @@ const NoteState = (props) => {
   )
 }
 export default NoteState;
+
+
