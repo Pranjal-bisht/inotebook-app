8eccc60bfd35d1438cc221e6fe14850bfeca74f2
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-11-06 10:33:09 +0530

    Added Add component to iNotebook

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,8 @@
 import React from 'react'
-
 export const Alert = (props) => {
     return (
         <div>
-            <div class="alert alert-primary" role="alert">
+            <div className="alert alert-primary" role="alert">
                 {props.message}
             </div>
         </div>
